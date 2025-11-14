df59337fe8b37311ca0633546c50353bd0abcce8
Taqey <atakieeldeen@gmail.com>
2025-11-15 01:21:46 +0200

    Add support ticket feature to application
    Removed CartController and HelpPageController methods.
    Introduced SubmitTicket method in HelpPageController.
    Registered ISubmitTicket service in Program.cs.
    Added SendEmailPlain method to IEmailSender.
    Extended TblUser with Tickets property.
    Implemented TicketRepository in UnitOfWork.
    Created Ticket entity and related database migration.
    Introduced DTOs for ticket submission and sending.
    Implemented SubmitTicket class for ticket handling.

---
+++ Backend/Cartify.Infrastructure/Migrations/20251114214537_a.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cartify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class a : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IssueCategory = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TicketStatus = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ClosedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TblUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_AspNetUsers_TblUserId",
+                        column: x => x.TblUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_TblUserId",
+                table: "Tickets",
+                column: "TblUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
